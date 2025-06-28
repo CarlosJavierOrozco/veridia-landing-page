7575dd0a2789db4d279d307ab0aa960558cd045e
Carlos J. Orozco <carlosjorozco.jw@gmail.com>
2025-06-28 13:18:59 -0300

    Menu hamburguesa completo. Original por jonMicha

---
+++ script.js
@@ -0,0 +1,7 @@
+import menuHamburguesa from "./menu_hamburguesa.js";
+
+const d = document
+
+d.addEventListener("DOMContentLoaded",e=>{
+    menuHamburguesa(".btn-menu",".panel",".nav-links a")
+})
